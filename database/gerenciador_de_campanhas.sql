807f5874186bf4b51da0bbcc93142f6e10bb29c9
Leandro Velez <leandrovelez55@gmail.com>
2023-07-10 14:50:39 -0300

    Atualizado o arquivo sql do banco

--- database/gerenciador_de_campanhas.sql
+++ database/gerenciador_de_campanhas.sql
@@ -14,6 +14,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Copiando estrutura do banco de dados para gerenciador_de_campanhas
+CREATE DATABASE IF NOT EXISTS `gerenciador_de_campanhas` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `gerenciador_de_campanhas`;
+
 -- Copiando estrutura para tabela gerenciador_de_campanhas.campanhas
 CREATE TABLE IF NOT EXISTS `campanhas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
