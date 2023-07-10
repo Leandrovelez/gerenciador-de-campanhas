36868520fc76eefd95f33fa2d0793bfa4863f8f7
Leandro Velez <leandrovelez55@gmail.com>
2023-07-09 21:14:32 -0300

    Adicionado validação nos campos de empresa

--- controllers/CompanyController.php
+++ controllers/CompanyController.php
@@ -42,24 +42,30 @@ class CompanyController {
      * @return json
      */        
     public function createCompany($data) {
-        $company = new Company();
-        $company->setRazaoSocial($data['razao_social']);
-        $company->setCnpj($data['cnpj']);
-        $company->setEndereco($data['endereco']);
-        $company->setNomeResponsavel($data['nome_responsavel']);
-        $company->setEmail($data['email']);
-        $company->setTelefone($data['telefone']);
-
-        if($company->getCompanyByCnpj()){
-            return json_encode("Já existe uma empresa cadastrada com esse CNPJ");
-        } else if($company->getCompanyByTelefone()){
-            return json_encode("Já existe uma empresa cadastrada com esse telefone");
-        } else {
-            $result = $company->createCompany();
-
-            return json_encode("Empresa cadastrada com sucesso");
-        }
         
+        $validate = $this->validateInputs($data);
+
+        if($validate){
+            return json_encode($validate);
+        } else {
+            $company = new Company();
+            $company->setRazaoSocial($data['razao_social']);
+            $company->setCnpj($data['cnpj']);
+            $company->setEndereco($data['endereco']);
+            $company->setNomeResponsavel($data['nome_responsavel']);
+            $company->setEmail($data['email']);
+            $company->setTelefone($data['telefone']);
+
+            if($company->getCompanyByCnpj()){
+                return json_encode("Já existe uma empresa cadastrada com esse CNPJ");
+            } else if($company->getCompanyByTelefone()){
+                return json_encode("Já existe uma empresa cadastrada com esse telefone");
+            } else {
+                $result = $company->createCompany();
+
+                return json_encode("Empresa cadastrada com sucesso");
+            }
+        }
     }
 
     /**
@@ -69,40 +75,45 @@ class CompanyController {
      * @return json
      */       
     public function updateCompany($id, $data) {
-        $company = new Company();
-        $company->setId($id);
-        $company->setRazaoSocial($data['razao_social']);
-        $company->setCnpj($data['cnpj']);
-        $company->setEndereco($data['endereco']);
-        $company->setNomeResponsavel($data['nome_responsavel']);
-        $company->setEmail($data['email']);
-        $company->setTelefone($data['telefone']);
+        $validate = $this->validateInputs($data);
 
-        $companyExists = $company->getCompanyById();
+        if($validate){
+            return json_encode($validate);
+        } else{
+            $company = new Company();
+            $company->setId($id);
+            $company->setRazaoSocial($data['razao_social']);
+            $company->setCnpj($data['cnpj']);
+            $company->setEndereco($data['endereco']);
+            $company->setNomeResponsavel($data['nome_responsavel']);
+            $company->setEmail($data['email']);
+            $company->setTelefone($data['telefone']);
 
-        if(!empty($companyExists)){
-            
-            $cnpjInUse = $company->getCompanyByCnpj();
-            $telefoneInUse = $company->getCompanyByTelefone();
-            
-            if($cnpjInUse && $cnpjInUse[0]['id'] !=  $id){
-                return json_encode("Já existe uma empresa com esse CNPJ");
-            } else if($telefoneInUse && $telefoneInUse[0]['id'] !=  $id){
-                return json_encode("Já existe uma empresa com esse telefone");
-            } else {    
+            $companyExists = $company->getCompanyById();
 
-                $result = $company->updateCompany();
-        
-                if($result){
-                    return json_encode($company->getCompanyById());
-                }
+            if(!empty($companyExists)){
                 
-                return json_encode("Erro ao atualizar a empresa");
-            }
-        } else {
-            return json_encode("Empresa não encontrada");
-        }        
+                $cnpjInUse = $company->getCompanyByCnpj();
+                $telefoneInUse = $company->getCompanyByTelefone();
+                
+                if($cnpjInUse && $cnpjInUse[0]['id'] !=  $id){
+                    return json_encode("Já existe uma empresa com esse CNPJ");
+                } else if($telefoneInUse && $telefoneInUse[0]['id'] !=  $id){
+                    return json_encode("Já existe uma empresa com esse telefone");
+                } else {    
 
+                    $result = $company->updateCompany();
+            
+                    if($result){
+                        return json_encode($company->getCompanyById());
+                    }
+                    
+                    return json_encode("Erro ao atualizar a empresa");
+                }
+            } else {
+                return json_encode("Empresa não encontrada");
+            } 
+        }
     }
 
     /**
@@ -129,6 +140,72 @@ class CompanyController {
         } else {
             return json_encode("Empresa não encontrada");
         }  
+    }
+
+    /**
+     * Validates the inputes given in data
+     *
+     * @param array $data 
+     * @return string
+     */
+    public function validateInputs($data){
+    
+        if(!isset($data['razao_social'])){
+            return "O campo razao_social é obrigatório";
+        }
+        if(is_numeric($data['razao_social'])){
+            return "O campo razao_social deve ser uma string";
+        }
+        if(strlen($data['razao_social']) > 250){
+            return "O campo razao_social deve ter no máximo 250 caracteres";
+        }
+
+        if(!isset($data['cnpj']) || empty($data['cnpj'])){
+            return "O campo cnpj é obrigatório";
+        }
+        if(!preg_match('/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/', $data['cnpj'])){
+            return "O CNPj informado é inválido";
+        }
+
+        if(!isset($data['endereco']) || empty($data['endereco'])){
+            return "O campo endereco é obrigatório";
+        }
+        if(is_numeric($data['endereco'])){
+            return "O campo endereco deve ser uma string";
+        }
+        if(strlen($data['endereco']) > 250){
+            return "O campo endereco deve ter no máximo 250 caracteres";
+        }
+
+        if(!isset($data['nome_responsavel']) || empty($data['nome_responsavel'])){
+            return "O campo nome_responsavel é obrigatório";
+        }
+        if(is_numeric($data['nome_responsavel'])){
+            return "O campo nome_responsavel deve ser uma string";
+        }
+        if(strlen($data['nome_responsavel']) > 250){
+            return "O campo nome_responsavel deve ter no máximo 250 caracteres";
+        }
+
+        if(!isset($data['email'])  || empty($data['email'])){
+            return "O campo email é obrigatório";
+        }
+        if(is_numeric($data['email'])){
+            return "O campo email deve ser uma string";
+        }
+        if(strlen($data['email']) > 250){
+            return "O campo email deve ter no máximo 250 caracteres";
+        }
+
+        if(!isset($data['telefone']) || empty($data['telefone'])){
+            return "O campo telefone é obrigatório";
+        }
+        if(is_numeric($data['telefone'])){
+            return "O campo telefone deve ser uma string";
+        }
+        if(!preg_match('/^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/', $data['telefone'])){
+            return "O campo telefone é inválido";
+        }
     }
 }
 
